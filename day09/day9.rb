fc7645f86333d25dccdbfd0fa0286b1e951a69d4
Melinda Weathers <melindaweathers@gmail.com>
2022-12-09 22:18:48 -0500

    day 9 part 2

--- day09/day9.rb
+++ day09/day9.rb
@@ -1,10 +1,13 @@
 require 'set'
 
 class RopeBridge
-  def initialize(filename)
-    @headx = @heady = @tailx = @taily = 1000
+  def initialize(filename, tailsize = 1)
+    starting_coord = 1000
+    @tailsize = tailsize
+    @snakex = Array.new(tailsize + 1){ starting_coord }
+    @snakey = Array.new(tailsize + 1){ starting_coord }
     @tail_visited = Set.new
-    @tail_visited << [@tailx, @taily]
+    @tail_visited << [@snakex.last, @snakey.last]
     @filename = filename
   end
 
@@ -19,25 +22,31 @@ class RopeBridge
   def run_move(dir, count)
     count.times do
       case dir
-      when 'L' then @headx -= 1
-      when 'R' then @headx += 1
-      when 'U' then @heady -= 1
-      when 'D' then @heady += 1
+      when 'L' then @snakex[0] -= 1
+      when 'R' then @snakex[0] += 1
+      when 'U' then @snakey[0] -= 1
+      when 'D' then @snakey[0] += 1
       end
 
-      touching = (@headx - @tailx).abs <= 1 && (@heady - @taily).abs <= 1
-      tol = (!touching && @headx != @tailx && @heady != @taily) ? 0 : 1
+      1.upto(@tailsize) do |idx|
+        touching = (@snakex[idx-1] - @snakex[idx]).abs <= 1 && (@snakey[idx-1] - @snakey[idx]).abs <= 1
+        tol = (!touching && @snakex[idx-1] != @snakex[idx] && @snakey[idx-1] != @snakey[idx]) ? 0 : 1
 
-      @tailx -= 1 if @tailx - @headx > tol
-      @tailx += 1 if @headx - @tailx > tol
-      @taily -= 1 if @taily - @heady > tol
-      @taily += 1 if @heady - @taily > tol
+        @snakex[idx] -= 1 if @snakex[idx] - @snakex[idx-1] > tol
+        @snakex[idx] += 1 if @snakex[idx-1] - @snakex[idx] > tol
+        @snakey[idx] -= 1 if @snakey[idx] - @snakey[idx-1] > tol
+        @snakey[idx] += 1 if @snakey[idx-1] - @snakey[idx] > tol
+      end
 
       # puts "moved #{dir}. Head - #{@headx},#{@heady}, Tail - #{@tailx}, #{@taily}"
-      @tail_visited << [@tailx, @taily]
+      @tail_visited << [@snakex.last, @snakey.last]
     end
   end
 end
 
 puts RopeBridge.new('input-test.txt').run_all
 puts RopeBridge.new('input.txt').run_all
+
+puts RopeBridge.new('input-test.txt', 9).run_all
+puts RopeBridge.new('input-test2.txt', 9).run_all
+puts RopeBridge.new('input.txt', 9).run_all
